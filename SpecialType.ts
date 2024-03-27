0ed75b44a96c95b9295297355ae043edefe9ef5a
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2024-03-27 15:44:28 +0530

    special types typescript

---
+++ SpecialType.ts
@@ -0,0 +1,9 @@
+//1.type:any:- this disables the type checking.
+
+// let u=true;
+// u="string";// error type string is not assignable to type boolean
+// console.log(u);
+
+let v:any="sanidhya";
+v=true
+console.log(v);
