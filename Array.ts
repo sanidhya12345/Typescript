98d010c9f0438bfca9659a3731cba4660e3513b6
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2024-03-27 15:48:59 +0530

    typescript array

---
+++ Array.ts
@@ -0,0 +1,12 @@
+let variable:string[]=[];
+variable.push("sanidhya varshney");
+variable.push("sandy");
+variable.push("hello world");
+variable.push("maje chal rahe hain");
+console.log(variable);
+
+//printing the elements one by one in typescript using for loop
+console.log(variable.length);
+for(let i=0;i<variable.length;i++){
+    console.log(variable[i])
+}
