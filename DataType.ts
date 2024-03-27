2a3794b6be32e973da08739c15c09170314c5140
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2024-03-27 15:38:40 +0530

    data type

---
+++ DataType.ts
@@ -0,0 +1,9 @@
+//explicit type declaration
+
+let firstname:string="Sanidhya Varshney";
+console.log(firstname);
+
+//explicit type declaration
+
+let firstnameImplicit="sanidhya varshney";
+console.log(firstnameImplicit);
